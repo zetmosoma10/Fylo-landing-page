db3413811acb6343e20659d687daf4da52fb9f4f
Zet Mosoma <zet10mokone@gmail.com>
2023-08-15 10:17:52 +0200

    build input component

---
+++ index.js
@@ -0,0 +1,19 @@
+const input = document.getElementById("input");
+const errorText = document.getElementById("error-text");
+
+// input.addEventListener("input", function () {
+//   input.classList.remove("invalid");
+//   errorText.style.display = "block";
+// });
+
+// input.addEventListener("blur", function () {
+//   if (!input.validity.valid) {
+//     // Remove styles
+//     errorText.style.display = "none";
+//     input.classList.remove("invalid");
+//     errorText.style.display = "block";
+//     input.classList.add("invalid");
+//   } else {
+//     // Add styles
+//   }
+// });
